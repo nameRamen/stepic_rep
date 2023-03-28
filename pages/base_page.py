b8e2650fc6c54c6635c81e2e8ff3aff28085a678
roman <romansecin474@gmail.com>
2023-03-28 21:40:16 +0700

    added base_page.py Initial class

---
+++ pages/base_page.py
@@ -0,0 +1,32 @@
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class Initial:
+    def __init__(self, driver, url):
+        self.driver = driver
+        self.url = url
+
+    def open(self):
+        self.driver.get(self.url)
+
+    def element_is_visible(self, locator, timeout=5):  # Ожидаем видимости одного элемента
+        return wait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
+
+    def element_are_visible(self, locator, timeout=5):  # Ожидаем видимость нескольких
+        return wait(self.driver, timeout).until(EC.visibility_of_all_elements_located(locator))
+
+    def element_is_present(self, locator, timeout=5):  # Проверяем по DOM дереву один элемент
+        return wait(self.driver, timeout).until(EC.presence_of_element_located(locator))
+
+    def element_are_present(self, locator, timeout=5):  # Проверяем по DOM дереву несколько
+        return wait(self.driver, timeout).until(EC.presence_of_all_elements_located(locator))
+
+    def element_is_not_visible(self, locator, timeout=5):  # Элемент невидим
+        return wait(self.driver, timeout).until(EC.invisibility_of_element_located(locator))
+
+    def element_is_clickable(self, locator, timeout=5):  # Проверяем на кликабельность
+        return wait(self.driver, timeout).until(EC.element_to_be_clickable(locator))
+
+    def go_to_element(self, element):  # Скролл до элемента
+        self.driver.execute_script("arguments[0].scrollIntoView;", element)
