e4fbf51d15a7aa5eaf3b9d12007f4759ed73bf2a
roman <romansecin474@gmail.com>
2023-03-29 00:08:07 +0700

    deleted unittest test

--- tests/selenium_tests.py
+++ tests/selenium_tests.py
@@ -1,33 +1,11 @@
-import unittest
-
-from selenium.webdriver.common.by import By
-import time
 from pages.selenium_course import UnitLesson
-from selenium import webdriver
 
 
-class TestAbs(unittest.TestCase):
-    def test_reg_unit(self):
-        browser = webdriver.Chrome()
-        browser.get("http://suninjuly.github.io/registration2.html")
-        input1 = browser.find_element(By.CSS_SELECTOR, '[placeholder="Input your name"]')
-        input1.send_keys("Roman Sechin")
-        input2 = browser.find_element(By.CSS_SELECTOR, '[placeholder="Input your email"]')
-        input2.send_keys("RomanSechin@gmail.com")
-        button = browser.find_element(By.CSS_SELECTOR, "button")
-        button.click()
-        time.sleep(1)
-        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
-        welcome_text = welcome_text_elt.text
-        self.assertEqual(welcome_text, "Congratulations! You have successfully registered!", "Doesn't match")
+class TestSeleniumCourse:
 
-
-if __name__ == "__main__":
-    unittest.main()
-
-    # def test_registration(self, driver):
-    #     unittests_lesson = UnitLesson(driver, "http://suninjuly.github.io/registration2.html")
-    #     unittests_lesson.open()
-    #     actual_text = unittests_lesson.registration_required_fields()
-    #     expected_text = "Congratulations! You have successfully registered!"
-    #     self.assertEqual(actual_text, expected_text, "Actual text doesn't match with expected text")
+    def test_registration(self, driver):
+        unittests_lesson = UnitLesson(driver, "http://suninjuly.github.io/registration2.html")
+        unittests_lesson.open()
+        actual_text = unittests_lesson.registration_required_fields()
+        expected_text = "Congratulations! You have successfully registered!"
+        assert actual_text == expected_text, "Actual text doesn't match with expected text"
